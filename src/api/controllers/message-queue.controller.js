86b65c086da630dfb519b83b7008a37006693268
Roee Benhous <roee_b@optimove.com>
2024-07-22 20:05:49 +0300

    Implement controller and routes.

--- src/api/controllers/message-queue.controller.js
+++ src/api/controllers/message-queue.controller.js
@@ -1,17 +1,41 @@
+import MessageQueueService from "../../domain/services/message-queue.service.js";
+import logger from "../../common/logger/console-logger.js";
+
 class MessageQueueController {
-  async getQueueMessagesWithTimeout(req, res) {
-    const { queue_name } = req.params; // todo: add request validation
-    const { timeout } = req.query; // todo: add request validation
-    console.log(`MessageQueueController >> Getting messages from queue [${queue_name}] with timeout [${timeout}]`); // todo: change to logger
-    
-    res.sendStatus(200);
+  async addMessageToQueue(req, res) {
+    const {queue_name} = req?.params; // todo: add request validation
+    const message = req?.body || "";
+
+    logger.info(`MessageQueueController >> Adding message to queue [${queue_name}]`);
+    const messageQueueService = new MessageQueueService();
+    await messageQueueService.init();
+    const result = await messageQueueService.addMessage(queue_name, message);
+
+    if (result.success) {
+      logger.info(`MessageQueueController >> success with msg [${result.message}]`);
+    } else {
+      logger.error(`MessageQueueController >> error with msg [${result.message}]`);
+    }
+
+    return result;
   }
   
-  async addMessageToQueue(req, res) {
-    const { queue_name } = req.params; // todo: add request validation
-    console.log(`MessageQueueController >> Adding message to queue [${queue_name}]`); //todo: change to logger
+  async getQueueMessagesWithTimeout(req) {
+    const {queue_name} = req?.params; // todo: add request validation
+    const {timeout} = req?.query; // todo: add request validation
+
+    logger.info(`MessageQueueController >> Getting messages from queue [${queue_name}] with timeout [${timeout}]`);
+    const messageQueueService = new MessageQueueService();
+    await messageQueueService.init();
+    const result = await messageQueueService.getMessage(queue_name, timeout)
+
+    if (result.success) {
+      logger.info(`MessageQueueController >> success with msg [${result.message}]`);
+    } else {
+      logger.error(`MessageQueueController >> error with msg [${result.error}]`);
+    }
     
-    res.sendStatus(201);
+    return result;
   }
 }
 

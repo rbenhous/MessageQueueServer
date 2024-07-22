f89cc8a3079d96ba64d81b0b249b4316d5a3bcc0
Roee Benhous <roee_b@optimove.com>
2024-07-22 15:00:54 +0300

    restore logger interface.

---
+++ src/common/logger/logger.interface.js
@@ -0,0 +1,15 @@
+class LoggerInterface {
+  info(message) {
+    throw new Error('Method "info()" must be implemented.');
+  }
+
+  warning(message) {
+    throw new Error('Method "warning()" must be implemented.');
+  }
+
+  error(message) {
+    throw new Error('Method "error()" must be implemented.');
+  }
+}
+
+module.exports = LoggerInterface;

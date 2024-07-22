c4614f57ae89bf6c19569bb891dd6ff82452736a
Roee Benhous <roee_b@optimove.com>
2024-07-22 20:19:55 +0300

    Add jsDoc to semaphore.

--- src/infra/common/transcations.semaphore.js
+++ src/infra/common/transcations.semaphore.js
@@ -1,6 +1,11 @@
 import {Semaphore} from "async-mutex";
 
 class TransactionsSemaphore {
+  /**
+   * This class is a singleton that provides a semaphore to control
+   * atomic transactions in the repositories,
+   * so we avoid race conditions and corrupted data.
+   */
   constructor() {
     this.message_queues_semaphore = new Semaphore(1);
   }
